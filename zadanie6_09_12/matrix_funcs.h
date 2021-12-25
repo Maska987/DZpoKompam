2d9a10d36ccd2232a731d5c1be2913acad037971
Maska987 <75728401+Maska987@users.noreply.github.com>
2021-12-25 15:26:24 +0300

    Create matrix_funcs.h

---
+++ zadanie6_09_12/matrix_funcs.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "matrix_funcs.h"
+
+void print_matrix(int** matrix, int rows, int cols);
+void random_matrix(int** matrix, int rows, int cols);
+int** create_matrix(int rows, int cols);
+void delete_matrix(int** matrix, int rows, int cols);
