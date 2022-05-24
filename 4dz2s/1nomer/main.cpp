f3477e628f0cc645295cc78d5d138bde8ac3c0ae
Maska987 <75728401+Maska987@users.noreply.github.com>
2022-05-24 13:20:45 +0300

    Create main.cpp

---
+++ 4dz2s/1nomer/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "List.h"
+
+int main()
+{
+    elem* list = nullptr;
+
+    add_unique(list, 1);
+    add_unique(list, 2);
+    add_unique(list, 3);
+    add_unique(list, 2);
+
+    insert_unique(list, 0, 0);
+    insert_unique(list, 0, 2);
+
+    show(list);
+}
